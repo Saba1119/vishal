cb02b732cf5bbbdc65dfc2493c57acf90ce1fdf8
Saba1119 <100400271+Saba1119@users.noreply.github.com>
2023-04-28 16:49:12 +0530

    Update application-start.sh

--- scripts/application-start.sh
+++ scripts/application-start.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 echo "starting go application"
-/home/ubuntu/goapp/user-app > /dev/null 2> /dev/null < /dev/null &
+# /home/ubuntu/goapp/user-app > /dev/null 2> /dev/null < /dev/null &
+cd /home/ubuntu/goapp/user-app
+./user-app &
